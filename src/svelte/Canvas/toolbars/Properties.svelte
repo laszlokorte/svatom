63f0a8aecc6f2fb3fec0e7da11acd5a1e80e5352
Laszlo Korte <laszlokorte@users.noreply.github.com>
2024-07-19 11:40:31 +0200

    style properties bar

--- src/svelte/Canvas/toolbars/Properties.svelte
+++ src/svelte/Canvas/toolbars/Properties.svelte
@@ -1,13 +1,17 @@
 <fieldset>
 	<legend>Properties</legend>
 
-	<div>
-		<label>Fill Color<input type="color" value="#00aaff" /></label>
-		<label>Stroke Color<input type="color" value="#0044aa" /></label>
-		<label>Text Color<input type="color" value="#003355" /></label>
-	</div>
-	<div>
-		<label
+	<div class="property-bar">
+		<label class="property-field"
+			>Fill Color<input type="color" value="#00aaff" /></label
+		>
+		<label class="property-field"
+			>Stroke Color<input type="color" value="#0044aa" /></label
+		>
+		<label class="property-field"
+			>Text Color<input type="color" value="#003355" /></label
+		>
+		<label class="property-field"
 			>Font size
 			<select>
 				<option>6</option>
@@ -21,7 +25,7 @@
 				<option>22</option>
 			</select>
 		</label>
-		<label
+		<label class="property-field"
 			>Font Family
 			<select>
 				<option>Arial</option>
@@ -30,7 +34,7 @@
 			</select>
 		</label>
 
-		<label
+		<label class="property-field"
 			>Stroke Width
 			<input
 				type="number"
@@ -41,7 +45,7 @@
 			/>
 		</label>
 
-		<label
+		<label class="property-field"
 			>Opacity
 			<input
 				type="range"
@@ -53,26 +57,33 @@
 			/>
 		</label>
 
-		<label
+		<label class="property-field"
 			>Function
 			<input type="text" style="width: 6em" />
 		</label>
 
-		<div>
-			Alignment:
-			<label><input type="radio" name="alignment" checked /> Left</label>
-			<label><input type="radio" name="alignment" /> Center</label>
-			<label><input type="radio" name="alignment" /> Right</label>
+		<div class="property-field-filled">
+			<span class="property-field-label">Text Align:</span>
+			<label class="icon-label"
+				><input type="radio" name="alignment" checked /> Left</label
+			>
+			<label class="icon-label"
+				><input type="radio" name="alignment" /> Center</label
+			>
+			<label class="icon-label"
+				><input type="radio" name="alignment" /> Right</label
+			>
 		</div>
 
-		<div>
-			Text Style:
-			<label><input type="checkbox" /> Bold</label>
-			<label><input type="checkbox" /> Italic</label>
-			<label><input type="checkbox" /> Underline</label>
-			<label><input type="checkbox" /> Strike</label>
+		<div class="property-field-filled">
+			<span class="property-field-label">Text Style:</span>
+			<label class="icon-label"><input type="checkbox" /> Bold</label>
+			<label class="icon-label"><input type="checkbox" /> Italic</label>
+			<label class="icon-label"><input type="checkbox" /> Underline</label
+			>
+			<label class="icon-label"><input type="checkbox" /> Strike</label>
 		</div>
-		<label
+		<label class="property-field"
 			>Dashes
 			<select>
 				<option>Solid</option>
@@ -82,10 +93,68 @@
 				<option>Dash-Dotted</option>
 			</select>
 		</label>
-		<div>
-			Arrows:
-			<label><input type="checkbox" /> Start</label>
-			<label><input type="checkbox" /> End</label>
+		<div class="property-field-filled">
+			<span class="property-field-label">Arrows:</span>
+			<label class="icon-label"><input type="checkbox" /> Start</label>
+			<label class="icon-label"><input type="checkbox" /> End</label>
 		</div>
 	</div>
 </fieldset>
+
+<style>
+	.property-bar {
+		display: flex;
+		flex-wrap: wrap;
+		gap: 4px;
+		align-items: stretch;
+	}
+
+	.property-field {
+		background: #f0f0f0;
+		display: flex;
+		align-items: center;
+		gap: 0.5em;
+		padding: 0.2em 0.2em 0.2em 0.5em;
+	}
+
+	.property-field-filled {
+		background: #f0f0f0;
+		display: flex;
+		align-items: stretch;
+		gap: 2px;
+		padding: 2px;
+	}
+
+	input[type="color"] {
+		width: 1.5em;
+		height: 1.5em;
+		padding: 0;
+		border: 0;
+		cursor: pointer;
+	}
+
+	label {
+		cursor: pointer;
+	}
+
+	.icon-label {
+		display: flex;
+		align-items: center;
+		padding: 0 0.4em;
+		color: #777;
+	}
+
+	.icon-label > input {
+		display: none;
+	}
+
+	.icon-label:has(:checked) {
+		background: #ee3f10;
+		color: #fff;
+	}
+
+	.property-field-label {
+		align-self: center;
+		padding: 0 0.5em;
+	}
+</style>
